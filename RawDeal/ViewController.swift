0460c9e5c4cd72ada76b31447e219dbef30db359
Bibhas Bhattacharya <bibhas2@gmail.com>
2018-03-12 19:53:45 -0400

    Initial Commit

---
+++ RawDeal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RawDeal
+//
+//  Created by Bibhas Bhattacharya on 3/12/18.
+//  Copyright © 2018 Bibhas Bhattacharya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
