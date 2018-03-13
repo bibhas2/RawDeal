39933565d756082efac91b729b5e30ce3c78ba04
Bibhas Bhattacharya <bibhas2@gmail.com>
2018-03-13 10:31:32 -0400

    Shiow camera controller by default

--- RawDeal/AppDelegate.swift
+++ RawDeal/AppDelegate.swift
@@ -13,9 +13,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        
+        let controller = CameraController()
+
+        window!.rootViewController = controller
+        window!.makeKeyAndVisible()
+        
         return true
     }
 
@@ -40,7 +46,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
 
 }
 
